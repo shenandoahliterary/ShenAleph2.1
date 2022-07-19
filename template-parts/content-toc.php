7b2474f18ca720adb1233ff8248f8e729a73ebcb
Jeff Barry <10522393+jeffreybarry@users.noreply.github.com>
2022-07-19 10:48:20 -0400

    Update content-toc.php
    Added placeholders for artist name and artwork on TOC.

--- template-parts/content-toc.php
+++ template-parts/content-toc.php
@@ -15,7 +15,7 @@ $upload_path =  $uploads['baseurl'];
 		<div class="text-center">
 <img class="" src="<?php echo $upload_path ?>/2022/06/572x700.jpg">
 		</div>
-<p class="d-flex justify-content-center"><em>Pink Room</em>,&nbsp;&nbsp; <a href="">Kathleen Olson-Janjic</a></p>
+<p class="d-flex justify-content-center"><em>[atwork title]</em>,&nbsp;&nbsp; <a href="">[artist name]</a></p>
 	</div>
 </div>
 
