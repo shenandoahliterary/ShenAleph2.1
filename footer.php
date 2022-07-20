3a0302f7266c87ee75aa581ea3555fce00190497
jjham1 <63023490+jjham1@users.noreply.github.com>
2022-07-20 19:26:34 -0400

    social media images

--- footer.php
+++ footer.php
@@ -14,7 +14,15 @@
 	</div><!-- #content -->
 
 	<footer>
-		<div id="footer-above"></div>
+		<div class="row">
+			<div id="footer-above">
+				<p class="text-right">
+					<a href="https://www.facebook.com/ShenandoahLiterary"><i class="fab fa-facebook-square"></i></a> 
+					<a href="https://www.instagram.com/shenandoah_literary"><i class="fab fa-instagram"></i></a> 
+					<a href="https://twitter.com/ShenandoahWLU"><i class="fab fa-twitter"></i></a>   
+				</p>
+			</div>
+		</div>
 
 		<img src="https://shenandoahliterary.org/681/files/2018/12/Shenandoah-1600.jpg" class="img-fluid" alt="Shenandoah Volume 68, Number 1" itemprop="logo">
 
