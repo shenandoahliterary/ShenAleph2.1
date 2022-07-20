c3faf8fb0c056b03268f977aae869ebf159c79c9
jjham1 <63023490+jjham1@users.noreply.github.com>
2022-07-20 19:21:05 -0400

    id

--- footer.php
+++ footer.php
@@ -14,7 +14,7 @@
 	</div><!-- #content -->
 
 	<footer>
-		<div class="footer-above">text</div>
+		<div id="footer-above">text</div>
 
 		<img src="https://shenandoahliterary.org/681/files/2018/12/Shenandoah-1600.jpg" class="img-fluid" alt="Shenandoah Volume 68, Number 1" itemprop="logo">
 
