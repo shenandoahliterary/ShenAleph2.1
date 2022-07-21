a76d63785933e449c373cc3eb0bf44660561e052
jjham1 <63023490+jjham1@users.noreply.github.com>
2022-07-20 20:11:58 -0400

    right align

--- footer.php
+++ footer.php
@@ -22,12 +22,10 @@
 				<a href="https://twitter.com/ShenandoahWLU"><i class="fab fa-twitter"></i></a>   
 			</div>
  
-			<div class="col" id="images">
-				<p class="text-end">
-					<a href = "https://shenandoahliterary.org/"><img class="shen-logo-footer" src="https://shenandoahliterary.org/681/files/2018/12/shen-w175.png"></a>
-					<a href = "https://www.wlu.edu/"><img src="https://shenandoahliterary.org/681/files/2018/12/wlu-w300.png"></a>
-					<a href = "https://www.clmp.org/"><img src="https://shenandoahliterary.org/681/files/2018/12/clmp.png" href = "https://www.clmp.org/"></a>
-				</p>
+			<div class="col align-items-end" id="images">
+				<a href = "https://shenandoahliterary.org/"><img class="shen-logo-footer" src="https://shenandoahliterary.org/681/files/2018/12/shen-w175.png"></a>
+				<a href = "https://www.wlu.edu/"><img src="https://shenandoahliterary.org/681/files/2018/12/wlu-w300.png"></a>
+				<a href = "https://www.clmp.org/"><img src="https://shenandoahliterary.org/681/files/2018/12/clmp.png" href = "https://www.clmp.org/"></a>
 			</div>
 
 		</div>
