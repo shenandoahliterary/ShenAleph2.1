f5f70ce0a0ac95804f06eca01077dbdc282c1f1c
jjham1 <63023490+jjham1@users.noreply.github.com>
2022-07-21 00:42:50 -0400

    text

--- footer.php
+++ footer.php
@@ -22,7 +22,7 @@
 				<a href="https://twitter.com/ShenandoahWLU"><i class="fab fa-twitter"></i></a>   
 			</div>
  
-			<div class="col" id="images" style="background-color:blue">
+			<div class="col text-end" id="images" style="background-color:blue">
 				<a href = "https://shenandoahliterary.org/"><img class="shen-logo-footer" src="https://shenandoahliterary.org/681/files/2018/12/shen-w175.png"></a>
 				<a href = "https://www.wlu.edu/"><img src="https://shenandoahliterary.org/681/files/2018/12/wlu-w300.png"></a>
 				<a href = "https://www.clmp.org/"><img src="https://shenandoahliterary.org/681/files/2018/12/clmp.png" href = "https://www.clmp.org/"></a>
