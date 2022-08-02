54990ac93e9682bf5f699f111c4bf448317d1758
jjham1 <63023490+jjham1@users.noreply.github.com>
2022-08-01 22:17:16 -0400

    changing div placement

--- footer.php
+++ footer.php
@@ -28,9 +28,9 @@
 				<a class="wl-logo" href = "https://www.wlu.edu/"><img src="https://shenandoahliterary.org/681/files/2018/12/wlu-w300.png"></a>
 			</div>
 
+			<img src="<?php echo get_stylesheet_directory_uri(); ?>/shen-header-new.svg" class="img-fluid" alt="Shenandoah Volume 68, Number 1" itemprop="logo">
+		
 		</div>
-
-		<img src="<?php echo get_stylesheet_directory_uri(); ?>/shen-header-new.svg" class="img-fluid" alt="Shenandoah Volume 68, Number 1" itemprop="logo">
 
 	</footer><!-- #colophon -->
 
