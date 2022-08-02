ee5d415813b738f4f999eb34beb3cde23dbaaa63
jjham1 <63023490+jjham1@users.noreply.github.com>
2022-08-01 23:25:45 -0400

    adding bg

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@
 
 	<footer>
 		<section class="footer-above">
-			<div class="row">
+			<div class="row" style="background-color: lightpink">
 
 				<div class="col images">
 					<a href="https://www.facebook.com/ShenandoahLiterary"><i class="fab fa-facebook-square" style="padding-left:15px"></i></a> 
