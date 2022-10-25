62031cacb287ee622e88140710e00034d5526f55
jjham1 <julieham03@gmail.com>
2022-10-24 20:55:56 -0400

    adding comics to php

--- template-parts/content-toc.php
+++ template-parts/content-toc.php
@@ -38,7 +38,7 @@ $upload_path =  $uploads['baseurl'];
 		</div>
 	</div>
 		
-	<div class="row">
+	<div class=
 		<?php
 			remove_all_filters('posts_orderby');
 			$fiction_args = array(
@@ -136,7 +136,7 @@ $upload_path =  $uploads['baseurl'];
 			
 		<?php
 		remove_all_filters('posts_orderby');
-		$fiction_args = array(
+		$novel_excerpt_args = array(
 			'category_name' => 'novel-excerpt',
 			'order' => 'ASC',
 			'meta_key' => 'TOC_order',
@@ -145,10 +145,10 @@ $upload_path =  $uploads['baseurl'];
 			'nopaging' => 'true',
 
 		);
-		$fiction_loop = new WP_Query($fiction_args);
+		$novel_excerpt_loop = new WP_Query($novel_excerpt_args);
 			$authornames = array();
 
-				while ($fiction_loop->have_posts()) : $fiction_loop->the_post();
+				while ($novel_excerpt_loop->have_posts()) : $novel_excerpt_loop->the_post();
 					$this_author= get_post_meta($post->ID, 'author_lastname', true);
 					$this_author_id =get_the_author_meta('ID');
 					$authornames[$this_author_id] = $this_author;
@@ -171,12 +171,12 @@ $upload_path =  $uploads['baseurl'];
 				);
 		?>
 		<?php
-				$fiction_loop_single = new WP_Query($args);
+				$novel_excerpt_loop_single = new WP_Query($args);
 
 				$i = 0;
 				//open paragraph for title(s)/author
 				echo "<p>";
-					while ($fiction_loop_single->have_posts()) : 				$fiction_loop_single->the_post();
+					while ($novel_excerpt_loop_single->have_posts()) : 				$novel_excerpt_loop_single->the_post();
 					//for each author, print title, title, author
 		?>
 
@@ -211,7 +211,7 @@ $upload_path =  $uploads['baseurl'];
 			
 		<?php
 		remove_all_filters('posts_orderby');
-		$fiction_args = array(
+		$nonfiction_args = array(
 			'category_name' => 'nonfiction',
 			'order' => 'ASC',
 			'meta_key' => 'TOC_order',
@@ -220,10 +220,10 @@ $upload_path =  $uploads['baseurl'];
 			'nopaging' => 'true',
 
 		);
-		$fiction_loop = new WP_Query($fiction_args);
+		$nonfiction_loop = new WP_Query($nonfiction_args);
 			$authornames = array();
 
-				while ($fiction_loop->have_posts()) : $fiction_loop->the_post();
+				while ($nonfiction_loop->have_posts()) : $nonfiction_loop->the_post();
 					$this_author= get_post_meta($post->ID, 'author_lastname', true);
 					$this_author_id =get_the_author_meta('ID');
 					$authornames[$this_author_id] = $this_author;
@@ -246,12 +246,13 @@ $upload_path =  $uploads['baseurl'];
 				);
 		?>
 		<?php
-				$fiction_loop_single = new WP_Query($args);
+				$nonfiction_loop_single = new WP_Query($args);
 
 				$i = 0;
 				//open paragraph for title(s)/author
 				echo "<p>";
-					while ($fiction_loop_single->have_posts()) : 				$fiction_loop_single->the_post();
+					while ($nonfiction_loop_single->have_posts()) :
+					    $nonfiction_loop_single->the_post();
 					//for each author, print title, title, author
 		?>
 
@@ -287,7 +288,7 @@ $upload_path =  $uploads['baseurl'];
 			
 		<?php
 		remove_all_filters('posts_orderby');
-		$fiction_args = array(
+		$poetry_args = array(
 			'category_name' => 'poetry',
 			'order' => 'ASC',
 			'meta_key' => 'TOC_order',
@@ -296,10 +297,10 @@ $upload_path =  $uploads['baseurl'];
 			'nopaging' => 'true',
 
 		);
-		$fiction_loop = new WP_Query($fiction_args);
+		$poetry_loop = new WP_Query($poetry_args);
 			$authornames = array();
 
-		while ($fiction_loop->have_posts()) : $fiction_loop->the_post();
+		while ($poetry_loop->have_posts()) : $poetry_loop->the_post();
 			$this_author= get_post_meta($post->ID, 'author_lastname', true);
 			$this_author_id =get_the_author_meta('ID');
 			$authornames[$this_author_id] = $this_author;
@@ -322,13 +323,13 @@ $upload_path =  $uploads['baseurl'];
 				);
 		?>
 		<?php
-				$fiction_loop_single = new WP_Query($args);
+				$poetry_loop_single = new WP_Query($args);
 
 				$i = 0;
 				//open paragraph for title(s)/author
 				echo "<p>";
-				while ($fiction_loop_single->have_posts()) : 				
-					$fiction_loop_single->the_post();
+				while ($poetry_loop_single->have_posts()) :
+					$poetry_loop_single->the_post();
 				//for each author, print title, title, author
 		?>
 
@@ -350,6 +351,97 @@ $upload_path =  $uploads['baseurl'];
 		?>
 
 	</div> <!-- close row -->
+
+	<div class="row justify-content-start"> <!-- open row for comics -->
+    		<div class="TOC-column">
+    			<h3>Comics</h3>
+    		</div>
+    	</div>
+
+    	<div class="row">
+    		<?php
+    			remove_all_filters('posts_orderby');
+    			$comics_args = array(
+    				'category_name' => 'comics',
+    				'order' => 'ASC',
+    				'meta_key' => 'TOC_order',
+    				'orderby' => 'meta_value_num',
+    				'meta_type' => 'NUMERIC',
+    				'nopaging' => 'true',
+    			);
+    			$comics_loop = new WP_Query($comics_args);
+    			$authornames = array();
+
+    			while ($comics_loop->have_posts()) : $comics_loop->the_post();
+    				$this_author= get_post_meta($post->ID, 'author_lastname', true);
+    				$this_author_id =get_the_author_meta('ID');
+    				$authornames[$this_author_id] = $this_author;
+
+    			//print statement of title and author just below worked but put each work and author separately
+
+    			endwhile;
+
+    			//group posts by author
+
+    			foreach ($authornames as $author_id=>$author_lastname) {
+    				$args = array(
+    					'category_name' => 'comics',
+    					'author' => $author_id,
+    					'orderby' => 'date',
+    					'order' => 'asc',
+    					'nopaging' => 'true'
+    				);
+
+    				//start WP loop
+    				$comics_loop_single = new WP_Query($args);
+
+    				$i = 0;
+
+    				//open paragraph for title(s)/author
+    				echo "<p>";
+    				while ($comics_loop_single->have_posts()) :
+    					$comics_loop_single->the_post();
+    					//for each author, print title,  author
+
+    		?>
+
+    				<a href="<?php the_permalink(); ?>">
+
+    		<?php the_title(); ?>
+
+    				</a><br/>
+
+    		<?php
+    				//check for author's note
+
+    				$custom_fields = get_post_custom();
+    				$has_author_note = $custom_fields['has_author_note'];
+
+    				$i++;
+
+    				endwhile;
+    				$custom_fields_test = get_post_custom();
+    				$has_author_note_test = $custom_fields_test['has_author_note'];
+
+    				if (! empty($has_author_note)) {
+    					$author_note_url = site_url();
+
+    					//echo "test: $has_author_note_test[0]";
+    					echo <<<URLLINK
+
+    					<a href="$author_note_url/$has_author_note[0]/">Author's Note</a><br />
+    					URLLINK;
+    				}
+    		?>
+
+    				<span class="author_name"><?php the_author(); ?> </span>
+
+    		<?php
+    					wp_reset_postdata();
+    			}
+    		?>
+
+    	</div> <!-- closes row -->
 
 	<p>&nbsp;</p>
 
