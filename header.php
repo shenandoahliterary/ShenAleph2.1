df19b2c61e05a663b6fe3ab03b05ac138cf00981
jjham1 <julieham03@gmail.com>
2022-10-27 00:22:55 -0400

    taking out row

--- header.php
+++ header.php
@@ -28,7 +28,7 @@
 <!-- start Bootstrap container -->
 <div id="page" class="site">
 
-	<section class="topbanner-noborder container-fluid" id="typelogo-container">
+	<section class="topbanner-noborder container-fluid typelogo-container">
 
 		<div class="row">
             <img src="<?php echo get_stylesheet_directory_uri(); ?>/favicon.jpg" alt="Shenandoah Logo" itemprop="logo">
@@ -37,13 +37,13 @@
 			</p>
 		</div>
 
-		<div class="row">
-			<a href="https://shenandoahliterary.org/721/" rel="home" itemprop="url">
-<!--				<img src="--><?php //echo get_stylesheet_directory_uri(); ?><!--/shen-header-new.svg" style="z-index:1" class="img-fluid" alt="Shenandoah Volume 68, Number 1" itemprop="logo">-->
-
-				<!-- <img src="https://shenandoahliterary.org/681/files/2018/12/Shenandoah-1600.jpg" class="img-fluid" alt="Shenandoah Volume 68, Number 1" itemprop="logo"> -->
-			</a>
-		</div>
+<!--		<div class="row">-->
+<!--			<a href="https://shenandoahliterary.org/721/" rel="home" itemprop="url">-->
+<!--<!--				<img src="-->--><?php ////echo get_stylesheet_directory_uri(); ?><!--<!--/shen-header-new.svg" style="z-index:1" class="img-fluid" alt="Shenandoah Volume 68, Number 1" itemprop="logo">-->-->
+<!---->
+<!--				<!-- <img src="https://shenandoahliterary.org/681/files/2018/12/Shenandoah-1600.jpg" class="img-fluid" alt="Shenandoah Volume 68, Number 1" itemprop="logo"> -->-->
+<!--			</a>-->
+<!--		</div>-->
 
 		<div class="row nav-bar-row" style="padding-top:15px; background-color:rgb(103,173,168)">
 			<div class="col-md-6">
