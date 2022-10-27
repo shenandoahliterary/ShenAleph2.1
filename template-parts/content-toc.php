9f1e2bfa63285521d2dc153f1061b36a1d2f9b67
jjham1 <julieham03@gmail.com>
2022-10-27 02:59:13 -0400

    Update content-toc.php

--- template-parts/content-toc.php
+++ template-parts/content-toc.php
@@ -214,83 +214,83 @@
                         }
                 ?>
             </div> <!-- close row -->
-        <!---->
-        <!--	<span class="text-center p-section-break">▴&nbsp;▴&nbsp;▴</span>-->
-        <!---->
-        <!--	<div class="row justify-content-start"> <!-- opens row for nonfiction -->-->
-        <!---->
-        <!--		<div class="TOC-column">-->
-        <!--			<h3>Nonfiction</h3>-->
-        <!--		</div>-->
-        <!--	</div> <!-- close row -->-->
-        <!--		-->
-        <!--	<div class="row">-->
-        <!--			-->
-        <!--		--><?php
-        //		remove_all_filters('posts_orderby');
-        //		$nonfiction_args = array(
-        //			'category_name' => 'nonfiction',
-        //			'order' => 'ASC',
-        //			'meta_key' => 'TOC_order',
-        //			'orderby' => 'meta_value_num',
-        //			'meta_type' => 'NUMERIC',
-        //			'nopaging' => 'true',
-        //
-        //		);
-        //		$nonfiction_loop = new WP_Query($nonfiction_args);
-        //			$authornames = array();
-        //
-        //				while ($nonfiction_loop->have_posts()) : $nonfiction_loop->the_post();
-        //					$this_author= get_post_meta($post->ID, 'author_lastname', true);
-        //					$this_author_id =get_the_author_meta('ID');
-        //					$authornames[$this_author_id] = $this_author;
-        //
-        //		//print statement of title and author just below worked but put each work and author separately
-        //		?>
-        <!---->
-        <!--		--><?php
-        //				endwhile;
-        //
-        //		//below groups posts by author
-        //
-        //			foreach ($authornames as $author_id=>$author_lastname) {
-        //					$args = array(
-        //				'category_name' => 'nonfiction',
-        //				'author' => $author_id,
-        //				'orderby' => 'date',
-        //				'order' => 'asc',
-        //				'nopaging' => 'true'
-        //				);
-        //		?>
-        <!--		--><?php
-        //				$nonfiction_loop_single = new WP_Query($args);
-        //
-        //				$i = 0;
-        //				//open paragraph for title(s)/author
-        //				echo "<p>";
-        //					while ($nonfiction_loop_single->have_posts()) :
-        //					    $nonfiction_loop_single->the_post();
-        //					//for each author, print title, title, author
-        //		?>
-        <!---->
-        <!--		<a href="--><?php //the_permalink(); ?><!--">-->
-        <!---->
-        <!--		--><?php //the_title(); ?>
-        <!--				-->
-        <!--		</a><br />-->
-        <!---->
-        <!--		--><?php
-        //				$i++;
-        //				endwhile;
-        //				//print author outside of the loop
-        //		?>
-        <!--				<span class="author_name">--><?php //the_author(); ?><!-- </span>-->
-        <!--		--><?php
-        //				wp_reset_postdata();
-        //			}
-        //		?>
-        <!---->
-        <!--	</div> <!-- close row -->-->
+
+        	<span class="text-center p-section-break">▴&nbsp;▴&nbsp;▴</span>
+
+        	<div class="row justify-content-start"> <!-- opens row for nonfiction -->
+
+        		<div class="TOC-column">
+        			<h3>Nonfiction</h3>
+        		</div>
+        	</div> <!-- close row -->
+
+        	<div class="row">
+
+        		<?php
+        		remove_all_filters('posts_orderby');
+        		$nonfiction_args = array(
+        			'category_name' => 'nonfiction',
+        			'order' => 'ASC',
+        			'meta_key' => 'TOC_order',
+        			'orderby' => 'meta_value_num',
+        			'meta_type' => 'NUMERIC',
+        			'nopaging' => 'true',
+
+        		);
+        		$nonfiction_loop = new WP_Query($nonfiction_args);
+        			$authornames = array();
+
+        				while ($nonfiction_loop->have_posts()) : $nonfiction_loop->the_post();
+        					$this_author= get_post_meta($post->ID, 'author_lastname', true);
+        					$this_author_id =get_the_author_meta('ID');
+        					$authornames[$this_author_id] = $this_author;
+
+        		//print statement of title and author just below worked but put each work and author separately
+        		?>
+
+        		<?php
+        				endwhile;
+
+        		//below groups posts by author
+
+        			foreach ($authornames as $author_id=>$author_lastname) {
+        					$args = array(
+        				'category_name' => 'nonfiction',
+        				'author' => $author_id,
+        				'orderby' => 'date',
+        				'order' => 'asc',
+        				'nopaging' => 'true'
+        				);
+        		?>
+        		<?php
+        				$nonfiction_loop_single = new WP_Query($args);
+
+        				$i = 0;
+        				//open paragraph for title(s)/author
+        				echo "<p>";
+        					while ($nonfiction_loop_single->have_posts()) :
+        					    $nonfiction_loop_single->the_post();
+        					//for each author, print title, title, author
+        		?>
+
+        		<a href="<?php the_permalink(); ?>">
+
+        		<?php the_title(); ?>
+
+        		</a><br />
+
+        		<?php
+        				$i++;
+        				endwhile;
+        				//print author outside of the loop
+        		?>
+        				<span class="author_name"><?php the_author(); ?> </span>
+        		<?php
+        				wp_reset_postdata();
+        			}
+        		?>
+
+        	</div> <!-- close row -->
         <!---->
         <!--	<span class="text-center p-section-break">▴&nbsp;▴&nbsp;▴</span>-->
         <!---->
