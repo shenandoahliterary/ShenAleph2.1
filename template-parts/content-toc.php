b5107b3323336b770883a48f62ec31ca979a98b6
jjham1 <julieham03@gmail.com>
2022-11-09 21:15:31 -0500

    Update content-toc.php

--- template-parts/content-toc.php
+++ template-parts/content-toc.php
@@ -4,7 +4,7 @@
  *
  */
 ?>
-<section class="container" style="background-color:#d6cdc6">
+<section class="container" style="background-color:#303B35">
     <div class="row cover-row">
 	    <?php
             $uploads = wp_upload_dir();
