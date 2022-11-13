41c893b67384e7be1b3a49b290ec307e1469b701
jjham1 <julieham03@gmail.com>
2022-11-12 22:29:16 -0500

    Update content-toc.php

--- template-parts/content-toc.php
+++ template-parts/content-toc.php
@@ -32,9 +32,9 @@
     </div>
 </section>
 
-    <div class="row d-flex justify-content-center">
+    <div class="row d-flex justify-content-center sticky-top" style="-1vw">
 
-        <div class="col-12 genreHeadings d-flex justify-content-around sticky-top" style="color:#b66631">
+        <div class="col-12 genreHeadings d-flex justify-content-around " style="color:#b66631">
                 <a href="guest">Border Crossing Narratives by Mai-Lee Chai</a>
                 <a href="#fiction">Fiction</a>
                 <a href="#novel-excerpt">Novel excerpts</a>
