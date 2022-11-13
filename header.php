8ee5982dd4598fc0cf9eff939d9a0f46540c601c
jjham1 <julieham03@gmail.com>
2022-11-12 21:36:27 -0500

    updating main nav bar

--- header.php
+++ header.php
@@ -47,8 +47,9 @@
 			</div>
 
 			<div class="d-flex flex-nowrap col-md-6 float-md-end nav-bar" role="navigation">
+				<a class="nav-link" style="padding-right:2.3vw" href="https://shenandoahliterary.org/721/">Current Issue</a>
 				<a class="nav-link" style="padding-right:2.3vw" href="https://shenandoahliterary.org/about/">About</a>
-				<a class="nav-link" style="padding-right:2.3vw" href="https://shenandoahliterary.org/issues/">Issues</a>
+				<a class="nav-link" style="padding-right:2.3vw" href="https://shenandoahliterary.org/issues/">Archive</a>
 				<a class="nav-link" style="padding-right:2.3vw" href="https://shenandoahliterary.org/submissions/">Submit</a>
 				<a class="nav-link" style="padding-right:2.3vw" href="https://shenandoahliterary.org/thepeak">The Peak</a>
 			</div>
