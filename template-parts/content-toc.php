cd6a159e2f29f1400fdb803a5aa0f129cef6e4af
jjham1 <julieham03@gmail.com>
2022-11-15 12:58:44 -0500

    Update content-toc.php

--- template-parts/content-toc.php
+++ template-parts/content-toc.php
@@ -53,7 +53,7 @@
             <a id="guest"></a>
             <!-- start of guest fiction -->
             <div class="row justify-content-start">
-                <div class="TOC-column" style="padding-top: 50px">
+                <div class="TOC-column" style="padding-top: 2em">
                         <h3>
                             Border Crossing Narratives<br>
                             guest-edited by May-lee Chai
