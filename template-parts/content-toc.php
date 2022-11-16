905ba572f836395113ab173d26fa3170f61b5795
jjham1 <julieham03@gmail.com>
2022-11-15 21:17:50 -0500

    editing formatting of closing credits

--- template-parts/content-toc.php
+++ template-parts/content-toc.php
@@ -551,7 +551,7 @@
 
         </div> <!-- closes column -->
 
-	<div class="row d-flex " style="margin-top:3vw; margin-bottom:3vw">
+	<div class="row d-flex justify-content-center closingCredits" style="">
 		<div class="col-12">
             <p class="TOC-subheadings"><a href="https://shenandoahliterary.org/721/editors-note/">Editor&rsquo;s Note</a><br /><span class="author_name">Beth Staples</span></p>
             <span class="TOC-subheadings"><p><a href="https://shenandoahliterary.org/721/masthead/">Masthead</a></span>
